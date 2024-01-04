4e5f8d3d7dd6782f27c793ceb0205877fb315676
旧城新月 <150812667+JCXYOZH@users.noreply.github.com>
2024-01-04 18:18:03 +0800

    Add files via upload

---
+++ 条件运算符.c
@@ -0,0 +1,13 @@
+/* 利用条件运算符的嵌套来完成此题：学习成绩>=90分的同学用A表示，60-89分之间的用B表示，60分以下的用C表示。*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int score;
+    scanf("%d", &score);
+    char grade = (score >= 90) ? 'A' : ((score >= 60) ? 'B' : 'C');
+    printf("%c\n", grade);
+    return 0;
+}
